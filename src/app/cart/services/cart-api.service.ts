6a092f064d078bb3e74191ef398e144ce493a103
Kacper Gotkowski <kgotkowski3@gmail.com>
2023-12-15 20:48:40 +0100

    Remove unused code

--- src/app/cart/services/cart-api.service.ts
+++ src/app/cart/services/cart-api.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../models/cart-item.model';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { ProductMapperService } from '../../products/services/product-mapper.service';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartApiService {
-  constructor(
-    private readonly http: HttpClient,
-    private readonly productMapper: ProductMapperService
-  ) {}
+  constructor(private readonly http: HttpClient) {}
 
   getAllCarts(): Observable<CartItem[]> {
     return this.http.get<CartItem[]>(`${environment.apiBaseUrl}carts`);
